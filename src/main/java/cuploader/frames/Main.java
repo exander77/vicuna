1a5cd128139a4bf967c64067f9b49bf45ffc9ef4
Radomír Polách <rp@t4d.cz>
2021-07-20 23:07:10 +0200

    change: sort alphabetically

--- src/main/java/cuploader/frames/Main.java
+++ src/main/java/cuploader/frames/Main.java
@@ -968,7 +968,8 @@ public final class Main extends javax.swing.JFrame
         /* Sort files by date */
         Collections.sort(files, new Comparator<File>() {
           public int compare(File f1, File f2) {
-            return Long.compare(f1.lastModified(), f2.lastModified());
+            return f1.getName().compareTo(f2.getName());
+            //return Long.compare(f1.lastModified(), f2.lastModified());
           }
         });
 

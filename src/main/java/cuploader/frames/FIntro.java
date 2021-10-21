f67c5ff38034b3b26b919e48b1c3d816c8cd2a6e
Radomír Polách <rp@t4d.cz>
2021-10-21 19:53:52 +0200

    fix: wiki-java library issue, version bump

--- src/main/java/cuploader/frames/FIntro.java
+++ src/main/java/cuploader/frames/FIntro.java
@@ -29,7 +29,7 @@ public class FIntro extends javax.swing.JFrame {
     bCancel.setVisible(false);
     
     Data.version = "1.24";
-    Data.minorVersion = ".7a";
+    Data.minorVersion = ".8a";
     Data.typeVersion = "Supported Edition";
     Data.date = "";
     

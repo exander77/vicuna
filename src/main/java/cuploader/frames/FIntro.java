3171309e4a4a950545ca9248d9f0b5f13b2f0a9e
Radomír Polách <rp@t4d.cz>
2022-01-18 21:23:30 +0100

    fix: smaller chunks to prevent bad upload

--- src/main/java/cuploader/frames/FIntro.java
+++ src/main/java/cuploader/frames/FIntro.java
@@ -31,7 +31,7 @@ public class FIntro extends javax.swing.JFrame {
     bCancel.setVisible(false);
     
     Data.version = "1.3";
-    Data.minorVersion = ".1a";
+    Data.minorVersion = ".2a";
     Data.typeVersion = "Supported Edition";
     Data.date = "";
     

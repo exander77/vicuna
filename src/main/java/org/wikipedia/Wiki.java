25d156d902d771f330a29d98847a123dc1d77b2f
Radomír Polách <rp@t4d.cz>
2022-01-23 21:19:19 +0100

    fix: chunk size note

--- src/main/java/org/wikipedia/Wiki.java
+++ src/main/java/org/wikipedia/Wiki.java
@@ -516,7 +516,7 @@ public class Wiki implements Comparable<Wiki>
             }
         }
         maxtries = Integer.parseInt(props.getProperty("maxretries", "2"));
-        log2_upload_size = Integer.parseInt(props.getProperty("loguploadsize", "21")); // 4 MB
+        log2_upload_size = Integer.parseInt(props.getProperty("loguploadsize", "21")); // 2 MB
         read_timeout_msec = Integer.parseInt(props.getProperty("readtimeout", "180000")); // 180 seconds
         cookies = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
         client = HttpClient.newBuilder()
